a4d36a2d3ced8e69e6e7c7710f55d12fa3cbf665
ekrigk01 <ekrigk01@ucy.ac.cy>
2024-11-21 16:22:24 +0200

    base j miso inv

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,15 @@
 function toggleSidebar() {
-    document.getElementById('sidebar').classList.toggle('open');
+    document.getElementById('sidebar').classList.toggle('close');
+}
+
+function toggleDropdown(event) {
+    event.preventDefault();
+    const parent = event.target.closest('.sidebar-dropdown');
+    parent.classList.toggle('open');
+}
+
+function toggleUserDropdown(event) {
+    event.preventDefault();
+    const parent = event.target.closest('.user-dropdown');
+    parent.classList.toggle('open');
 }
