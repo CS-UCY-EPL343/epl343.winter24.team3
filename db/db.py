3414b7f7b5e9e8ec86fa687d273e57286e813f1f
pgeorg0 <pgeorg04@ucy.ac.cy>
2024-11-23 15:24:41 +0200

    all queries completed, some adjustments

--- db/db.py
+++ db/db.py
@@ -1,21 +1,20 @@
 import sqlite3
 import hashlib
+import os
 
 # > connect if db exist - create if database doesn't.
 connection = sqlite3.connect('epl343.db')
 
-#needs uid also
-def search_inventory(query):
+def search_inventory(uid, query):
     # > create cursor to access the db.
     cursor = connection.cursor()
-    searchInv = """SELECT * FROM ENTRY WHERE NAME LIKE '%?%'"""
-    cursor.execute(searchInv, (query))
+    searchInv = """SELECT * FROM ENTRY WHERE UID = (?) AND NAME LIKE '%?%'"""
+    cursor.execute(searchInv, (uid, query))
     rows = cursor.fetchall()
     connection.close()
     return rows    
 
-#assume that uid is given as int?
-def getQuickSwitchUsers(uid):
+def get_quick_switch_users(uid):
     # > create cursor to access the db.
     cursor = connection.cursor()
     qsUsers = """SELECT U.USERNAME FROM USER U WHERE U.UID IN(SELECT Q.USER_OTHER FROM QUICK_ACCESS Q WHERE Q.UID = (?))"""
@@ -36,17 +35,18 @@ def get_user_uid(new_username):
 def validate_user(new_username, password):
     # > create cursor to access the db.
     cursor = connection.cursor()
-    validate = """SELECT PASS_HASHED FROM USER WHERE USERNAME = (?)"""
+    validate = """SELECT PASS_HASHED, SALT FROM USER WHERE USERNAME = (?)"""
     cursor.execute()
-    pw = cursor.fetchall()
+    values = cursor.fetchall()
+    pw, salt = values[0]
     connection.close()
-    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    hashed_password = hashlib.sha256(salt + password.encode()).hexdigest()
     if hashed_password == pw:
         return True
     else:
         return False    
 
-def addQuickSwitchUser(uid, new_username):
+def add_quick_switch_user(uid, new_username):
     # > create cursor to access the db.
     new_uid = get_user_uid(new_username)
     cursor = connection.cursor()
@@ -55,7 +55,7 @@ def addQuickSwitchUser(uid, new_username):
     connection.commit()
     connection.close()
 
-def isUsernameTaken(username):
+def user_exists(username):
     cursor = connection.cursor()
     getUid = """SELECT UID FROM USER WHERE USERNAME = (?)"""
     cursor.execute(getUid, (username))
@@ -67,15 +67,17 @@ def isUsernameTaken(username):
         return True
 
 def set_user(username, password):
-    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    #encrypt password
+    salt = os.urandom(16)
+    hashed_password = hashlib.sha256(salt + password.encode()).hexdigest()
     # > create cursor to access the db.
     cursor = connection.cursor()
-    newUser = """INSERT INTO USER (USERNAME, PASS_HASHED) VALUES (?, ?)"""
-    cursor.execute(newUser, (username, hashed_password))
+    newUser = """INSERT INTO USER (USERNAME, PASS_HASHED, SALT) VALUES (?, ?, ?)"""
+    cursor.execute(newUser, (username, hashed_password, salt))
     connection.commit()
     connection.close()
 
-def getInventoryData(uid):
+def get_inventory_data(uid):
     # > create cursor to access the db.
     cursor = connection.cursor()
     getInv = """SELECT * FROM ENTRY WHERE UID = (?) ORDER BY NAME DESC"""
@@ -84,7 +86,7 @@ def getInventoryData(uid):
     connection.close()
     return rows
 
-def getCategoryOptions(uid):
+def get_category_options(uid):
     # > create cursor to access the db.
     cursor = connection.cursor()
     getCat = """SELECT DISTINCT CATEGORY FROM ENTRY WHERE UID = (?)"""
@@ -93,7 +95,7 @@ def getCategoryOptions(uid):
     connection.close()
     return rows
 
-def getSupplierOptions(uid):
+def get_supplier_options(uid):
     # > create cursor to access the db.
     cursor = connection.cursor()
     getSup = """SELECT DISTINCT SUPPLIER FROM ENTRY WHERE UID = (?)"""
@@ -102,27 +104,73 @@ def getSupplierOptions(uid):
     connection.close()
     return rows
 
-#item_uid = entry_id?
-def updateEntry(entry_id, name, size, category, supplier, minreq, photo, qnt):
+def update_entry(entry_id, name, size, category, supplier, minreq, photo, qnt, unav): #COMP UNAVAILABLE
     cursor = connection.cursor()
-    updEntry = """UPDATE ENTRY SET NAME = (?), SIZE = (?), CATEGORY = (?), SUPPLIER = (?), MINREQ = (?), PHOTO = (?), QNT = (?) WHERE ENTRY_ID = (?)"""
-    cursor.execute(updEntry, (name, size, category, supplier, minreq, photo, qnt, entry_id))
+    old_qnt = get_quantity(entry_id)
+    updEntry = """UPDATE ENTRY SET NAME = (?), SIZE = (?), CATEGORY = (?), SUPPLIER = (?), MINREQ = (?), PHOTO = (?), QNT = (?), UNAVAILABLE = (?) WHERE ENTRY_ID = (?)"""
+    cursor.execute(updEntry, (name, size, category, supplier, minreq, photo, qnt, unav, entry_id))
     connection.commit()
     connection.close()
+    if old_qnt[0] > qnt:
+        add_log_ent(entry_id, (qnt - old_qnt[0]))
 
-def createEntry(name, size, category, supplier, minreq, photo, qnt):
+def create_entry(name, size, category, supplier, minreq, photo, qnt): #COMP UNAVAILABLE
     cursor = connection.cursor()
-    createEntry = """INSERT INTO ENTRY (NAME, SIZE, CATEGORY, SUPPLIER, MINREQ, PHOTO, QNT) VALUES (?, ?, ?, ?, ?, ?, ?)"""
+    createEntry = """INSERT INTO ENTRY (NAME, SIZE, CATEGORY, SUPPLIER, MINREQ, PHOTO, QNT, UNAVAIlABLE) VALUES (?, ?, ?, ?, ?, ?, ?, 'F')"""
     cursor.execute(createEntry, (name, size, category, supplier, minreq, photo, qnt))
     connection.commit()
     connection.close()
 
-#def getFilteredInventory(uid, category, supplier, qnt_filter):
+def get_filtered_inventory(uid, category, supplier, qnt_filter): 
+    cursor = connection.cursor()
 
-#def generateReport(uid):
+    getAll = """SELECT * FROM ENTRY WHERE UID = (?)"""
+    filterCat = """SELECT * FROM ENTRY WHERE UID = (?) AND CATEGORY = (?)"""
+    filterSup = """SELECT * FROM ENTRY WHERE UID = (?) AND SUPPLIER = (?)"""
+    filterUnav = """SELECT * FROM ENTRY WHERE UID = (?) AND UNAVAILABLE = 'T"""
+    filterZero = """SELECT * FROM ENTRY WHERE UID = (?) AND QNT = 0"""
+    filterBelowMin = """SELECT * FROM ENTRY WHERE UID = (?) AND (QNT<MINREQ)"""
+    filterCloseToMin = """SELECT * FROM ENTRY WHERE UID = (?) AND (QNT>MINREQ) AND (QNT<=MINREQ+5)"""
+    filterMany = """SELECT * FROM ENTRY WHERE UID = (?) AND (QNT>=MINREQ+5)"""
 
-#does a user also get the transactions they requested themselves??
-def getPendingTransactions(uid):
+    if category == '':
+        cursor.execute(getAll, (uid))
+    else:
+        cursor.execute(filterCat, (uid, category))
+    catFilter = cursor.fetchall()
+
+    if supplier == '':
+        cursor.execute(getAll, (uid))
+    else:
+        cursor.execute(filterSup, (uid, supplier))
+    supFilter = cursor.fetchall()
+
+    if qnt_filter == '':
+        cursor.execute(getAll, (uid))
+    elif qnt_filter == 'unavailable':
+        cursor.execute(filterUnav, (uid))
+    elif qnt_filter == 'zero':
+        cursor.execute(filterZero, (uid))
+    elif qnt_filter == 'below_minimum':
+        cursor.execute(filterBelowMin, (uid))
+    elif qnt_filter == 'close_to_minimum':
+        cursor.execute(filterCloseToMin, (uid))
+    elif qnt_filter == 'many':
+        cursor.execute(filterMany, (uid))
+    qntFilter = cursor.fetchall()
+
+    finalFilter = [x for x in catFilter if x in supFilter and x in qntFilter]
+    return finalFilter
+
+def generate_report(uid): 
+    cursor = connection.cursor()
+    report = """SELECT * FROM LOG WHERE (UID = (?)) AND (DATE <= datetime('now', 'localtime') AND (DATE <= datetime('now','-7 days', 'localtime'))"""
+    cursor.execute(report, (uid))
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
+def get_pending_transactions(uid):
     # > create cursor to access the db.
     cursor = connection.cursor()
     pendingTrans = """SELECT * FROM PENDING_TRANSACTIONS WHERE UID_ANS_TRANS = (?)"""
@@ -131,11 +179,7 @@ def getPendingTransactions(uid):
     connection.close()
     return rows
 
-#not needed 
-def user_exists(to_user):
-    return isUsernameTaken(to_user)
-
-def addTransaction(uid, to_uid, entry_id, qnt):
+def add_transaction(uid, to_uid, entry_id, qnt):
     cursor = connection.cursor()
     createTrans = """INSERT INTO PENDING_TRANSACTIONS (UID_REQ_TRANS, UID_ANS_TRANS, ENTRY_ID, QNT) VALUES (?, ?, ?, ?)"""
     cursor.execute(createTrans, (uid, to_uid, entry_id, qnt))
@@ -153,30 +197,58 @@ def transaction_exists(trans_id):
     else:
         return True
 
-#how should answer work?
-#def answertransaction(trans_id, answer):
-
-def IncreaseQuantity(entry_id, qnt):
+def answer_transaction(trans_id, answer):
     cursor = connection.cursor()
-    old_qnt = getQuantity(entry_id)
+    if answer:
+        getTrans = """SELECT UID_REQ_TRANS, UID_ANS_TRANS, ENTRY_ID, QNT FROM PENDING TRANSACTIONS WHERE TRANS_ID = (?)"""
+        cursor.execute(getTrans, (trans_id))
+        values = cursor.fetchall()
+        uidReq, uidAns, entry_id_req, qnt = values[0]
+        getAnsEid = """SELECT A.ENTRY_ID FROM ENTRY R, ENTRY A WHERE R.ENTRY_ID = (?) AND A.UID = (?) AND A.NAME = R.NAME AND A.SIZE = R.SIZE"""
+        cursor.execute(getAnsEid, (entry_id_req, uidAns))
+        values = cursor.fetchall()
+        entry_id_ans = values[0]
+        increase_quantity(entry_id_req, qnt)
+        decrease_quantity(entry_id_ans, qnt)
+    delTrans = """DELETE FROM PENDING_TRANSACTIONS WHERE TRANS_ID = (?)"""
+    cursor.execute(delTrans, (trans_id))
+    connection.commit()
+    connection.close()
+
+def increase_quantity(entry_id, qnt):
+    cursor = connection.cursor()
+    old_qnt = get_quantity(entry_id)
     incQnt = "UPDATE ENTRY SET QNT = (?) WHERE ENTRY_ID = (?)"
     cursor.execute(incQnt, ((int(old_qnt[0])+qnt), entry_id))
     connection.commit()
     connection.close()
+    add_log_ent(entry_id, qnt)
 
-#error handling if old qnt < qnt ?
-def DecreaseQuantity(entry_id, qnt):
+def decrease_quantity(entry_id, qnt): 
     cursor = connection.cursor()
-    old_qnt = getQuantity(entry_id)
+    old_qnt = get_quantity(entry_id)
+    if(old_qnt[0]<qnt):
+        raise ValueError("Value bigger than quantity in stock!")
     decQnt = "UPDATE ENTRY SET QNT = (?) WHERE ENTRY_ID = (?)"
     cursor.execute(decQnt, ((int(old_qnt[0])-qnt), entry_id))
     connection.commit()
     connection.close()
+    add_log_ent(entry_id, -(qnt))
 
-def getQuantity(entry_id):
+def get_quantity(entry_id):
     cursor = connection.cursor()
     getQnt = """SELECT QNT FROM ENTRY WHERE ENTRY_ID = (?)"""
     cursor.execute(getQnt, (entry_id))
     qnt = cursor.fetchall()
     connection.close()
     return qnt
+
+def add_log_ent(entry_id, qnt_dif): 
+    cursor = connection.cursor()
+    getUid = """SELECT UID FROM ENTRY WHERE ENTRY_ID = (?)"""
+    cursor.execute(getUid, (entry_id))
+    uid = cursor.fetchall()
+    updateLog = """INSERT INTO LOG (UID, ENTRY_ID, QNT_DIF, DATE_TIME) VALUES (?, ?, ?, datetime('now', 'localtime'))"""
+    cursor.execute(updateLog, (uid[0], qnt_dif, entry_id))
+    connection.commit()
+    connection.close()
